ee69a3f56dd65ca1c78d17382af208a87d7a8f1e
MSI <ing.victorcamacho@gmail.com>
2023-02-01 09:39:57 -0500

    se agrega propiedades y se soluciona issue sonar

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -1,12 +1,23 @@
 package org.acme;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Path("/hello")
 public class ExampleResource {
+
+    static Logger logger = Logger.getLogger(ExampleResource.class.getName());
+
+
+    @ConfigProperty(name="greeting")
+    private String greeting;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -14,4 +25,16 @@ public class ExampleResource {
 
         return "Hello RESTEasy";
     }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("custom/{name}")
+    public String customHello(@PathParam("name") String name) {
+
+        logger.log(Level.INFO, "logging: {0} {1}", new Object[] { "greeting", "Quarkus" });
+        logger.log(Level.INFO, "logging: {0} {1}", new Object[] { name, greeting });
+        return greeting +" " +  name +"  welcome";
+    }
+
+
 }
